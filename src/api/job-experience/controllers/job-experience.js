a99bc6248b05f48010b8ec5844ef1765995bec45
Wobetu Shiferaw <wobetushiferaw@gmail.com>
2024-11-08 14:05:19 +0300

    Basic API for my personal resume project completed.

---
+++ src/api/job-experience/controllers/job-experience.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * job-experience controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::job-experience.job-experience');
---
+++ src/api/job-experience/services/job-experience.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * job-experience service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::job-experience.job-experience');
